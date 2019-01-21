46c39d3700b9b399184d14f44e8919bb332ab09b
Nison Malayev <nmalayev@gmail.com>
2019-01-20 23:55:52 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,13 @@ class ProductsController < ApplicationController
   def add
     @product = params[:product]
     cart << @product
-    render :index
+    # render :index
+    redirect_to root_path
   end
 
   def index
+    # don't need anything here b/c this controller inherits from ApplicationController, which has
+    # helper_method :cart, making cart available to a view (index in this case)
   end
 
 end
